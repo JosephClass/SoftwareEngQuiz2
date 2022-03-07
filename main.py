f0f3a2ded18fae654196e3f772af9dbb96807b68
JosephClass <93022837+JosephClass@users.noreply.github.com>
2022-03-07 11:58:37 +0700

    Fixing some problem

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
+from curses import window
 import CaseStudy
 import os
+from tkinter import *
+window=Tk()
+# add widgets here
 
-AccessData = CaseStudy.StudiKasus2(host="localhost" , port=3306 , user="root" , password=os.environ['PASS_MYSQL'])
+window.title('Hello Python')
+window.geometry("300x200+10+20")
+window.mainloop()
+# AccessData = CaseStudy.StudiKasus2(host="localhost" , port=3306 , user="root" , password=os.environ['PASS_MYSQL'])
 
-print(AccessData.create_db("Tryout2"))
+# print(AccessData.create_db("Tryout2"))
 
-df = AccessData.import_csv("C:\\Users\\User-PC\\Desktop\\addresses.csv")
+# df = AccessData.import_csv("C:\\Users\\User-PC\\Desktop\\addresses.csv")
 
-# print(df.firstName)
+# AccessData.create_table("Tryout2","user",df)
 
-AccessData.create_table("Tryout2","user",df)
-
-print(AccessData.load_data("Tryout2","user"))
+# print(AccessData.load_data("Tryout2","user"))
 
